abfb62050bf3f77f4333dcf0a6bb6085e07bbce6
pinar.kizilarslan <pinar.kizilarslan@trendyol.com>
2023-06-15 18:14:34 +0300

    fix(badge): fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { ThemeProvider } from '@ergenekonyigit/restyle';
 export { default as theme } from './theme';
 
 export { default as Alert } from './components/Alert/Alert';
+export { default as Badge } from './components/Badge/Badge';
 export { default as Box } from './components/Box/Box';
 export { default as Button } from './components/Button/Button';
 export { default as Icon } from './components/Icon/Icon';
