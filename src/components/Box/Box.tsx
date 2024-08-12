e5946628619d888bf4904362d0d2279e3f0a50b2
Göksel Pırnal <gokselpirnal@gmail.com>
2024-08-12 11:56:31 +0300

    fix(box): sync testID and accessibilityLabel

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,6 +1,59 @@
-import { createBox } from '@ergenekonyigit/restyle';
+import {
+  backgroundColor,
+  opacity,
+  visible,
+  layout,
+  spacing,
+  border,
+  shadow,
+  position,
+  useRestyle,
+  spacingShorthand,
+  composeRestyleFunctions,
+  BoxProps as RestyleBoxProps,
+} from '@ergenekonyigit/restyle';
+import React from 'react';
+import { ViewProps as RNViewProps, View as RNView } from 'react-native';
 import { Theme } from '../../theme';
 
-const Box = createBox<Theme>();
+type RestyleProps = RestyleBoxProps<Theme>;
+
+const restyleFunctions = composeRestyleFunctions<Theme, RestyleProps>([
+  backgroundColor,
+  opacity,
+  visible,
+  layout,
+  spacing,
+  border,
+  shadow,
+  position,
+  spacingShorthand,
+]);
+
+export type BoxProps = RNViewProps & RestyleProps;
+
+const Box = ({ testID, accessibilityLabel, accessible, ...rest }: BoxProps) => {
+  const props = useRestyle(restyleFunctions, rest);
+
+  const testProps = React.useMemo(() => {
+    const result: RNViewProps = {
+      accessible: false,
+      accessibilityLabel: undefined,
+      testID: undefined,
+    };
+
+    if (!(testID || accessibilityLabel)) {
+      return result;
+    }
+
+    result.accessible = accessible ?? true;
+    result.accessibilityLabel = accessibilityLabel ?? testID;
+    result.testID = testID ?? accessibilityLabel;
+
+    return result;
+  }, [testID, accessibilityLabel, accessible]);
+
+  return <RNView {...props} {...testProps} />;
+};
 
 export default Box;
