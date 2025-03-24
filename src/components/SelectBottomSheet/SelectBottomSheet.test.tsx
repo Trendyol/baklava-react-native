f4cbca225968a0b70f5115437a7786e5dcf30939
pinar.kizilarslan <pinar.kizilarslan@trendyol.com>
2025-03-24 19:25:45 +0300

    feat(selectBottomSheet): added SelectBottomSheet component

---
+++ src/components/SelectBottomSheet/SelectBottomSheet.test.tsx
@@ -0,0 +1,169 @@
+import React from 'react';
+import { fireEvent, render } from '../../test-utils';
+import SelectBottomSheet from './SelectBottomSheet';
+import { FlagIconNameType } from '../FlagIcon/types';
+
+describe('SelectBottomSheet', () => {
+  const options = [
+    { id: '1', label: 'Option 1' },
+    { id: '2', label: 'Option 2' },
+    { id: '3', label: 'Option 3' },
+  ];
+
+  const mockOnSelect = jest.fn();
+  const defaultProps = {
+    options,
+    type: 'checkbox' as const,
+    onSelect: mockOnSelect,
+    visible: true,
+    closeButtonLabel: 'Close',
+    selectButtonLabel: 'Select',
+    flagIcon: 'turkey',
+  };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should render correctly', () => {
+    // when
+    const { getByTestId } = render(<SelectBottomSheet {...defaultProps} />);
+
+    //then
+    expect(getByTestId('selectBottomSheet')).toBeTruthy();
+  });
+
+  test('renders correctly with radio type', () => {
+    // when
+    const { getByTestId } = render(
+      <SelectBottomSheet {...defaultProps} type="radio" />,
+    );
+
+    //then
+    expect(getByTestId('selectBottomSheet')).toBeTruthy();
+  });
+
+  test('should render SelectBottomSheet when visible true', () => {
+    // when
+    const { toJSON, queryByTestId } = render(
+      <SelectBottomSheet {...defaultProps} />,
+    );
+
+    // then
+    expect(queryByTestId('selectBottomSheet')).not.toBeNull();
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  test('should render SelectBottomSheet when visible false', () => {
+    // when
+    const { toJSON, queryByTestId } = render(
+      <SelectBottomSheet {...defaultProps} visible={false} />,
+    );
+
+    // then
+    const modalWrapper = queryByTestId('selectBottomSheet');
+    expect(modalWrapper).toBeNull();
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  test('should not render SelectBottomSheet when visible is false', () => {
+    // when
+    const { toJSON, queryByTestId } = render(
+      <SelectBottomSheet {...defaultProps} />,
+    );
+
+    // then
+    expect(queryByTestId('modal-wrapper')).toBeNull();
+    expect(toJSON()).toMatchSnapshot();
+  });
+
+  test('should initialize with correct selected values', () => {
+    // when
+    const { getByTestId } = render(
+      <SelectBottomSheet {...defaultProps} selectedOption={['1', '2']} />,
+    );
+
+    // then
+    expect(
+      getByTestId('checkbox-selectBottomSheet-1').props.children[0][0].props
+        .variant,
+    ).toBe('checked');
+    expect(
+      getByTestId('checkbox-selectBottomSheet-2').props.children[0][0].props
+        .variant,
+    ).toBe('checked');
+    expect(
+      getByTestId('checkbox-selectBottomSheet-3').props.children[0][0].props
+        .variant,
+    ).toBe('unchecked');
+  });
+
+  test('should update selected values on checkbox select', () => {
+    // when
+    const { getByTestId } = render(<SelectBottomSheet {...defaultProps} />);
+    const checkbox = getByTestId('checkbox-selectBottomSheet-1');
+    fireEvent.press(checkbox);
+
+    // then
+    expect(
+      getByTestId('checkbox-selectBottomSheet-1').props.children[0][0].props
+        .variant,
+    ).toBe('checked');
+  });
+
+  test('should update selected values on checkbox unselect', () => {
+    // given
+    const { getByTestId } = render(
+      <SelectBottomSheet {...defaultProps} selectedOption={['1', '2']} />,
+    );
+    const checkbox = getByTestId('checkbox-selectBottomSheet-1');
+
+    // when
+    fireEvent.press(checkbox);
+
+    // then
+    expect(
+      getByTestId('checkbox-selectBottomSheet-1').props.children[0][0].props
+        .variant,
+    ).toBe('unchecked');
+    expect(mockOnSelect).toHaveBeenCalledWith(['2']);
+  });
+
+  test('should update selected values on radio button select', () => {
+    // when
+    const { getByTestId } = render(
+      <SelectBottomSheet {...defaultProps} type="radio" />,
+    );
+    const radioButton = getByTestId('radiobutton-selectBottomSheet-2');
+    fireEvent.press(radioButton);
+
+    // then
+    expect(mockOnSelect).toHaveBeenCalledWith(['2']);
+  });
+
+  test('should call onSelect with selected radio button value', () => {
+    // when
+    const { getByText } = render(
+      <SelectBottomSheet {...defaultProps} type="radio" />,
+    );
+    fireEvent.press(getByText('Option 3'));
+
+    // then
+    expect(mockOnSelect).toHaveBeenCalledWith(['3']);
+  });
+
+  test('should render flag icon if provided', () => {
+    // given
+    const optionsWithFlag = [
+      { id: '1', label: 'Option 1', flagIcon: 'turkey' as FlagIconNameType },
+    ];
+
+    // when
+    const { getByTestId } = render(
+      <SelectBottomSheet {...defaultProps} options={optionsWithFlag} />,
+    );
+
+    // then
+    expect(getByTestId('flag-icon').props.title).toBe('turkey');
+  });
+});
---
+++ src/components/SelectBottomSheet/SelectBottomSheet.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import { FlatList, ScrollView, StyleSheet } from 'react-native';
+import Box from '../Box/Box';
+import BottomSheet, { BottomSheetProps } from '../BottomSheet/BottomSheet';
+import Checkbox from '../Checkbox/Checkbox';
+import RadioButton from '../RadioButton/RadioButton';
+import { FlagIconNameType } from '../FlagIcon/types';
+
+interface Option {
+  id: string;
+  label: string;
+  flagIcon?: FlagIconNameType | null;
+}
+
+interface SelectBottomSheetProps extends BottomSheetProps {
+  options: Option[];
+  type: 'checkbox' | 'radio';
+  onSelect: (selectedValues: string[]) => void;
+  selectedOption?: string[];
+  maxVisibleItems?: number;
+}
+
+const SelectBottomSheet: React.FC<SelectBottomSheetProps> = ({
+  options,
+  type,
+  onSelect,
+  selectedOption,
+  maxVisibleItems = 6,
+  ...bottomSheetProps
+}) => {
+  const [selectedValues, setSelectedValues] = React.useState<string[]>(
+    selectedOption ?? [],
+  );
+
+  React.useEffect(() => {
+    if (Array.isArray(selectedOption)) {
+      setSelectedValues(selectedOption);
+    }
+  }, [selectedOption]);
+
+  const handleSelect = (value: string) => {
+    if (type === 'checkbox') {
+      setSelectedValues(prev =>
+        prev.includes(value) ? prev.filter(v => v !== value) : [...prev, value],
+      );
+    } else {
+      setSelectedValues([value]);
+    }
+  };
+
+  React.useEffect(() => {
+    onSelect(selectedValues);
+  }, [selectedValues, onSelect]);
+
+  const renderItem = ({ item, index }: { item: Option; index: number }) => {
+    return (
+      <Box key={item.id}>
+        {type === 'checkbox' ? (
+          <Checkbox
+            testID={`checkbox-selectBottomSheet-${item.id}`}
+            label={item.label}
+            flagIcon={item.flagIcon}
+            checked={selectedValues.includes(item.id)}
+            onPress={() => handleSelect(item.id)}
+          />
+        ) : (
+          <RadioButton
+            testID={`radiobutton-selectBottomSheet-${item.id}`}
+            label={item.label}
+            flagIcon={item.flagIcon}
+            selected={selectedValues.includes(item.id)}
+            onPress={() => handleSelect(item.id)}
+          />
+        )}
+        {index < options.length - 1 && (
+          <Box height={1} backgroundColor="borderColor" marginVertical="m" />
+        )}
+      </Box>
+    );
+  };
+
+  return (
+    <BottomSheet {...bottomSheetProps}>
+      <ScrollView
+        testID={'selectBottomSheet'}
+        contentContainerStyle={styles.flatListContainer}
+        horizontal
+        bounces={false}>
+        <FlatList
+          data={options}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+          nestedScrollEnabled
+          initialNumToRender={maxVisibleItems}
+          maxToRenderPerBatch={maxVisibleItems}
+          style={{ maxHeight: maxVisibleItems * 50 - 15 }}
+        />
+      </ScrollView>
+    </BottomSheet>
+  );
+};
+
+export default SelectBottomSheet;
+
+const styles = StyleSheet.create({
+  flatListContainer: {
+    flex: 1,
+    width: 'auto',
+  },
+});
